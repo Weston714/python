1d5166144c210ad9f4619ec117cfc28524cfdb7a
Weston714 <simonweston7@gmail.com>
2021-01-28 14:43:41 +0000

    French Fries

--- python.py
+++ python.py
@@ -3,8 +3,11 @@ Weston Simon
 1/28/21
 combo menue
 """
-
-Sandwich_type = input("What type of sandwich do you want? ")
+Sandwich = input("Do you want a Sandwich? ")
+if Sandwich == "Yes":
+    Sandwich_type = input("What type of sandwich do you want? ")
+elif Sandwich == "No":
+    Sandwich_type == "None"
 
 if Sandwich_type == "Chicken":
     print("Chicen $5.25")
@@ -17,6 +20,8 @@ elif Sandwich_type == "Beef":
 elif Sandwich_type == "Tofu":
     print("Tofu $5.25")
     meal_price = float(5.25)
+elif Sandwich_type == "None":
+    meal_price = float(0)
 
 else:
     print("invalid sandwich selection please restart the program and try again with one of the three sandwich types: Chicken, Beef, Tofu")
@@ -47,5 +52,44 @@ elif DrinkSize == "Large":
 elif DrinkSize == ("None"):
     DrinkPrice = float(0)
 
-Checkout = float(meal_price + DrinkPrice)
+Fries = input("Would you like French Fries? ")
+
+if Fries == "Yes":
+    FriesSize = input("What size of French Fries do you want? ")
+elif Fries == "No":
+    print("No fries sellected proceding to checkout.")
+    FriesSize = str("none")
+
+if FriesSize == "Small":
+    print("Small fries $1.00")
+    FriesPrice = float(1.00)
+    MegaFries = input("Would you like to mega size your Fries? ")
+    if MegaFries == "Yes":
+        FriesPrice = float(2.00)
+
+elif FriesSize == "Medium":
+    print("Medium Fries: $1.50")
+    FriesPrice = float(1.50)
+
+elif FriesSize == "Large":
+    print("Large Fries: $2.00")
+    FriesPrice = float(2.00)
+elif FriesSize == "none":
+    FriesPrice = float(0) 
+
+Ketchup = int(input("How many ketchup packets would you like?"))
+TotalKetchup = float(Ketchup * 0.25)
+
+if Sandwich == Drink == Fries == "Yes":
+    print("You recive our $1.00 discount for ordering a sandwich with fries and a drink.")
+    Discount = float(1.00)
+    DiscountRecived = (True)
+else:
+    Discount = float(0)
+    DiscountRecived = (False)
+
+Items = (Sandwich_type + DrinkSize + FriesSize + Ketchup)
+print(Items)
+
+Checkout = float(meal_price + DrinkPrice + FriesPrice + TotalKetchup)
 print("Total= $"+str(Checkout))
